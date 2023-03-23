1c61772c8f31ca8d19effca6d75858e130cbca43
emilemus <92602556+EmilioLem@users.noreply.github.com>
2023-03-23 16:59:09 -0600

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,36 @@ function frameRendering() {
   }
   
   //Every image array goes like... RGBA_RGBA_RGBA... right?
+
+  function whereIam(){
+    console.time("whereIam");
+    
+    
+    
+    console.timeEnd("whereIam");
+    setTimeout(whereIam, 500);
+  }
+  
+  function takePhotoSaveData(){
+    let dataM = dataO.slice();  //The data we will manipulate
+    for (let i = 1; i <= dataM.length/4; i++) {
+      dataM.splice(i, 1); //Takes out from i element, just 1 element.
+    }
+    console.log(dataM.length);
+
+    let msgImage = prompt("Introduzca una breve descripción del lugar que ve", "Aquí se puede ver...");
+    if(msgImage != null) localStorage.setItem(msgImage, JSON.stringify(dataM));
+  }
+
+  function exportData(){
+    for(let i=0; i<localStorage.length; i++){
+      alert(localStorage.key(i));
+    }
+  }
+  //.getItem, key, setItem, length, removeItem
   
   function invertColor() {
-    console.time("invertColor");
+    //console.time("invertColor");
     dataI = dataO.slice();  //The data we will manipulate
     let iDisplay = Ictx.createImageData(pW, pH);
     iData = iDisplay.data; //The array that will revive the data
@@ -22,5 +49,5 @@ function frameRendering() {
         
     Ictx.putImageData(iDisplay, 0, 0);
     console.timeEnd("invertColor");
-    setTimeout(invertColor, 3.33);
+    //setTimeout(invertColor, 3.33);
   }
